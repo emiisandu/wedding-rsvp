24711a93e7a59018fb563ff715cb757e9c250441
emiisandu <louise.sandu@gmail.com>
2026-02-08 18:41:11 +0200

    details section fix

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -11,20 +11,19 @@ export default function Details() {
   return (
     <div>
       <div className="relative max-w-4xl mx-auto
-    min-h-[150vh]
-    sm:min-h-[200vh]
-    [@media(min-height:900px)]:min-h-[140vh]
-    [@media(min-height:1200px)]:min-h-[120vh]">
-        <div className="sticky top-0  overflow-hidden z-40 bg-pink pt-3 w-[]">
-          <div className="flex flex-row items-center justify-center gap-4">
+                      min-h-[150vh]
+                      sm:min-h-[200vh]
+                      [@media(min-height:900px)]:min-h-[140vh]
+                      [@media(min-height:1200px)]:min-h-[120vh]">
+        <div className="sticky top-0  overflow-hidden z-40 bg-pink pt-3 w-[102%] ">
+          {/* <div className="flex flex-row items-center justify-center gap-4"> */}
             {/* <h2 className="text-2xl font-semibold mb-8 text-center font-monoton z-40 text-beige">
               Sinopsis
             </h2> */}
 
-            
-          </div>
 
-          <div className="flex items-center justify-center date-div text-align text-right ">
+
+          <div className="flex flex-row items-center justify-center date-div text-align text-right ">
             <p className="font-bold uppercase font-monoton
                           [font-size:clamp(1.8rem,7vw,3rem)]
                           [letter-spacing:clamp(0.12em,1.8vw,0.4em)]
@@ -32,24 +31,27 @@ export default function Details() {
                           text-center
                           whitespace-nowrap
                           max-w-full">
-                            
+
               25.07.26
             </p>
             <img
               className="mt-1 w-24 sm:w-28 md:w-32"
               src="/images/hydrangea.png"
-              alt="Theo & Didi"
+              alt="hydrangea"
             />
           </div>
+          <div className="flex flex-row items-center justify-center ">
+            <p className="text-1xl sm:text-2xl font-bold tracking-[0.1em] uppercase font-prata mt-2 flex justify-center pb-2 ">
+              <img
+                src="/images/pin.png"
+                alt=""
+                className=" w-4 sm:w-8 mr-2 object-cover"
+              />
+              FUNDU MOLDOVEI, SUCEAVA
+            </p>
+          </div>
+                    {/* </div> */}
 
-          <p className="text-1xl sm:text-2xl font-bold tracking-[0.1em] uppercase font-prata mt-2 flex justify-center pb-2">
-                <img
-                            src="/images/pin.png"
-                            alt=""
-                            className=" w-4 sm:w-8 mr-2 object-cover"
-                        />
-            FUNDU MOLDOVEI, SUCEAVA
-          </p>
         </div>
 
         <Parallax speed={-7}>
@@ -62,7 +64,7 @@ export default function Details() {
           </div>
         </Parallax>
 
-        <div className="location-div max-w-3xl bg-cover bg-center bg-no-repeat z-30 mt-10 mx-auto">
+        <div className="location-div relative max-w-3xl bg-cover bg-center bg-no-repeat z-30 mt-10 mx-auto">
           <NotebookSection
             title="ORA 14"
             children={`Iertăciune,
@@ -94,13 +96,17 @@ la_baciu`}
             />
           </div>
 
-          <Parallax speed={25}>
+          <Parallax
+            speed={25}
+            className="absolute left-0 right-0 bottom-20 z-50 flex justify-center pointer-events-none"
+          >
             <img
-              className="w-80 sm:w-76 md:w-84 lg:w-80 cartoon-couple z-50 absolute bottom-20"
+              className="w-80 sm:w-76 md:w-84 lg:w-80"
               src="/images/champagne.svg"
               alt="Theo & Didi"
             />
           </Parallax>
+
         </div>
       </div>
     </div>
